8654838e3d17958087799428a32c0a72aefc68c2
CT <c.timm@campus.tu-berlin.de>
2022-06-15 13:25:18 +0200

    add resolution of blog list task 4.1 and 4.2

---
+++ part4/blog-list/backend/index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const config = require('./utils/config');
+const app = require('./app');
+
+const server = http.createServer(app);
+
+const PORT = config.PORT;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
